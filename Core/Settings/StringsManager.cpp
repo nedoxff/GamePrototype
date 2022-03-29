5b060e866dd8d13f2a4c1a4e6506f1579c14b3bb
nedo <65343244+NedoProgrammer@users.noreply.github.com>
2022-03-29 14:37:57 +0300

    🔥 Remove everything from master

--- Core/Settings/StringsManager.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by nedo on 22.03.2022.
-//
-
-#include "StringsManager.hpp"
--- UI/Window/Window.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by nedo on 28.03.2022.
-//
-
-#include "Window.hpp"
-
-void Window::Load(const std::string &path) {
-
-}
